12dd8992464638087f4cf164aa13b19394d67e56
Sávio Mendes <saviomd@gmail.com>
2018-08-10 16:16:17 -0300

    Lesson 9

--- src/button.js
+++ src/button.js
@@ -1,20 +1,12 @@
 import { buttonSize, boxShadow } from './mixins';
 import { textColor, backgroundColor } from './variables';
 
-const cssString = `
-  .cssinjs-btn {
-    color: #fff;
-    background-color: green;
-    margin: 5px;
-    ${buttonSize({paddingY: '50px'})};
-    ${boxShadow('0px 4px 5px #666', '2px 6px 10px #999')};
-  }
-`;
-
 const css = {
   '.cssinjs-btn': {
       color: textColor,
-      background: backgroundColor
+      background: backgroundColor,
+      ...buttonSize(),
+      ...boxShadow('0px 4px 5px #666', '2px 6px 10px #999')
   }
 };
 
--- src/mixins.js
+++ src/mixins.js
@@ -1,16 +1,16 @@
 import { paddingYLg, paddingXLg, fontSizeLg, borderRadiusLg } from './variables';
 
-export const boxShadow = (...shadows) => `
-  box-shadow: ${shadows};
-`;
+export const boxShadow = (...shadows) => ({
+  'box-shadow': shadows
+});
 
 export const buttonSize = ({
   paddingY = paddingYLg,
   paddingX = paddingXLg,
   fontSize = fontSizeLg,
   borderRadius = borderRadiusLg
-} = {}) => `
-  padding: ${paddingY} ${paddingX};
-  font-size: ${fontSize};
-  border-radius: ${borderRadius};
-`;
+} = {}) => ({
+  padding: `${paddingY} ${paddingX}`,
+  'font-size': fontSize,
+  'border-radius': borderRadius
+});
